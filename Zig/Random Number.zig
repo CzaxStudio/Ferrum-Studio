a34bab98fcb56a66d6379b9816aac5a0bdb1571b
Hacker <minubgp@gmail.com>
2026-04-28 12:03:33 +0530

    Delete Zig directory

--- Zig/Random Number.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-
-pub fn main() void {
-    var prng = std.rand.DefaultPrng.init(12345);
-    const rand = prng.random();
-
-    const number = rand.intRangeAtMost(i32, 1, 100);
-    std.debug.print("Random: {}\n", .{number});
-}
